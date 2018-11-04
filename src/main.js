f78993b73ca620fd4390804c946a7d84936576c2
Anton Prokopev <a.prokopev@innopolis.ru>
2018-11-04 22:12:50 +0300

    Add kefetchup as dependency

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+// import { GenericApiClient } from 'kefetchup'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue'
@@ -6,6 +7,7 @@ import router from './router'
 import store from './store'
 
 Vue.use(ElementUI);
+// Vue.use(GenericApiClient);
 
 Vue.config.productionTip = false;
 
